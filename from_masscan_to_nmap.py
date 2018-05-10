67809c6fa1339ba5dbfcf907f7b5d1a4a072cc77
kiriknik <kiriknik@mail.ru>
2018-05-10 12:20:40 +0300

    Update from_masscan_to_nmap.py

--- from_masscan_to_nmap.py
+++ from_masscan_to_nmap.py
@@ -5,6 +5,7 @@ import argparse
 parser = argparse.ArgumentParser(add_help=True, description='From masscan XML to nmap scan')
 parser.add_argument('-x', '--xml', required=True, help='Path to masscan XML')
 parser.add_argument('-n', '--no_printer_scan', required=False, help='Please,dont scan printer',action='store_true', default=False)
+parser.add_argument('-a','--add_auth_and_safe_scripts',required=False, help='Please,dont scan printer',action='store_true', default=False)
 
 wtf={}
 args = parser.parse_args()
@@ -22,17 +23,22 @@ for child in root:
         wtf[ip].append(port)
 for element in wtf:
     if options['no_printer_scan']:
-	flag=0
-	for port_printer in printer_ports:
-		if port_printer in wtf[element]:flag=port_printer
-	if flag!=0:
-		print ("I found printer at "+str(element)+" with port " + str(flag))
-	else:
-    		string= str("nmap -sT -sV --version-all -sC --max-rate 15000 -Pn -T4 -oA results/"+element+" "+element+" -p"+str(wtf[element]).replace("['","").replace("']","").replace("'","").replace(" ",""))
-    		#print string
-		os.system(string)
+        flag=0
+        for port_printer in printer_ports:
+            if port_printer in wtf[element]:flag=port_printer
+        if flag!=0:
+            print ("I found printer at "+str(element)+" with port " + str(flag))
+        else:
+            if options['add_auth_and_safe_scripts']:
+                string= str("nmap -sT -sV --version-all -n --script '(default or safe or auth) and not (broadcast-listener or broadcast-ping or eap-info or targets-asn)' --max-rate 15000 -Pn -T4 -oA results/"+element+" "+element+" -p"+str(wtf[element]).replace("['","").replace("']","").replace("'","").replace(" ",""))
+            else:
+                string = str("nmap -sT -sV --version-all -sC --max-rate 15000 -Pn -T4 -oA results/" + element + " " + element + " -p" + str(wtf[element]).replace("['", "").replace("']", "").replace("'", "").replace(" ", ""))
+            print string
+		    #os.system(string)
     else:
-    	string= str("nmap -sT -sV --version-all -sC --max-rate 15000 -Pn -T4 -oA results/"+element+" "+element+" -p"+str(wtf[element]).replace("['","").replace("']","").replace("'","").replace(" ",""))
-    	#print string
-	os.system(string)
-
+        if options['add_auth_and_safe_scripts']:
+           string= str("nmap -sT -sV --version-all -n --script '(default or safe or auth) and not (broadcast-listener or broadcast-ping or eap-info or targets-asn)' --max-rate 15000 -Pn -T4 -oA results/"+element+" "+element+" -p"+str(wtf[element]).replace("['","").replace("']","").replace("'","").replace(" ",""))
+        else:
+            string = str("nmap -sT -sV --version-all -sC --max-rate 15000 -Pn -T4 -oA results/" + element + " " + element + " -p" + str(wtf[element]).replace("['", "").replace("']", "").replace("'", "").replace(" ", ""))
+        print string
+	#os.system(string)
